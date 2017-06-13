0d6a3304205ad9139b75a4525658ad74b871201f
Kung Ben <Kung Ben>
2017-06-13 14:38:51 +0800

    OK

--- src/app/admin/adminlayout/adminlayout.component.ts
+++ src/app/admin/adminlayout/adminlayout.component.ts
@@ -10,6 +10,12 @@ export class AdminlayoutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Initialize collapse button
+   ( $(".button-collapse") as any).sideNav('show');
+    // Initialize collapsible (uncomment the line below if you use the dropdown variation)
+    //$('.collapsible').collapsible();
+
+
   }
 
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -3,3 +3,14 @@ declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
+declare var Colors:any
+
+declare interface JQuery{
+  $:any;
+  plot:any;
+  easyPieChart:any;
+  datepicker:any;
+  vectorMap:any;
+  sideNav:any
+}
+
