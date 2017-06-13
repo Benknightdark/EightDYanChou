e1ead4d6c616fcd3540e802fab6178409352aabc
Kung Ben <Kung Ben>
2017-06-13 14:03:15 +0800

    OK

--- src/app/guard/account.guard.ts
+++ src/app/guard/account.guard.ts
@@ -2,14 +2,19 @@ import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import * as firebase from 'firebase/app';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Injectable()
 export class AccountGuard implements CanActivate {
-
-  constructor(private router:Router){}
+u;
+  constructor(private router:Router,private  afAuth: AngularFireAuth){}
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
+    state: RouterStateSnapshot) {//: Observable<boolean> | Promise<boolean> | boolean
+
+
+
+
 if( localStorage.getItem("token")){
   return true
 }else{
