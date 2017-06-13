328325cc1bdcf044fd25c02fb7e9195148b6e9fc
Kung Ben <Kung Ben>
2017-06-13 16:42:23 +0800

    ddd

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -10,18 +10,18 @@ import 'rxjs'
   styleUrls: ['./event.component.css']
 })
 export class EventComponent implements OnInit {
-ShowEventData:Observable<any>
+  ShowEventData: Observable<any>
   constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
-       this.ShowEventData= this.db.list("/EventData", {
-  query: {
-    orderByChild: 'CreateTime',
-  }
-}).map( (arr) =>  arr.reverse() );
+    this.ShowEventData = this.db.list("/EventData", {
+      query: {
+        orderByChild: 'CreateTime',
+      }
+    }).map((arr) => arr.reverse());
   }
   onShareEvent(url) {
-    window.open("https://www.facebook.com/sharer/sharer.php?u="+url+";src=sdkpreparse")
+    window.open("https://www.facebook.com/sharer/sharer.php?u=" + url + ";src=sdkpreparse")
   }
   onSignUpEvent(url) {
     window.open(url)
