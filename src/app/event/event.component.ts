b1fabe10269f8c56439332f1b492503622930fb2
Kung Ben <Kung Ben>
2017-06-13 10:33:11 +0800

    fsad

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -11,5 +11,11 @@ export class EventComponent implements OnInit {
 
   ngOnInit() {
   }
+  onShareEvent() {
+window.open("https://www.facebook.com/sharer/sharer.php?u=https://docs.google.com/forms/d/1juE7N44SuROXCPYpSEqGl9C7FAYPgL4zDfHEaoAHTpE/viewform?edit_requested=true;src=sdkpreparse")
+  }
+  onSignUpEvent() {
+    window.open("http://goo.gl/PJ1yub")
+  }
 
 }
