2aaa7dc3a6ba824b9dfce96b9245f25a975b49e7
Kung Ben <Kung Ben>
2017-06-13 16:28:10 +0800

    ddd

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase } from "angularfire2/database";
 import { Observable } from 'rxjs/Observable';
+// tslint:disable-next-line:import-blacklist
+import 'rxjs'
 
 @Component({
   selector: 'app-event',
@@ -12,13 +14,13 @@ ShowEventData:Observable<any>
   constructor(private db: AngularFireDatabase) { }
 
   ngOnInit() {
-       this.ShowEventData= this.db.list("/EventData")
+       this.ShowEventData= this.db.list("/EventData").map( (arr) =>  arr.reverse() );
   }
   onShareEvent(url) {
     window.open("https://www.facebook.com/sharer/sharer.php?u="+url+";src=sdkpreparse")
   }
   onSignUpEvent(url) {
-    window.open("url")
+    window.open(url)
   }
 
 }
